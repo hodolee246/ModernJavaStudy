5829668067c4a10384b6bdf81f7ca317d2398baa
inwoo <hodolee246@naver.com>
2020-10-29 17:57:05 +0900

    ch21 코드 추가

---
+++ src/test/java/com/example/modernjava/ch21/ch21.java
@@ -0,0 +1,29 @@
+package com.example.modernjava.ch21;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class ch21 {
+
+    @Test
+    public void genericParam() {
+
+        // 제네릭의 형식의 파라미터 생략가능
+        // Map<String, List<String>> myMap = new HashMap<String, List<String>>();
+        Map<String, List<String>> myMap = new HashMap<>();
+    }
+
+    @Test
+    public void genericWildCard() {
+        // 호환되지 않는 형식
+        // List<Number> numbers = new ArrayList<Integer>();
+        List<? extends Number> numbers = new ArrayList<Integer>();
+    }
+}
+
+
